096026538aaf6742e4d359850f5910436b477c0d
Andriy Tomkiv <andriy.tomkiv@gmail.com>
2017-02-08 17:16:33 +0200

    person

--- src/Person.java
+++ src/Person.java
@@ -1,3 +1,14 @@
 
 public class Person {
+
+    private String name;
+    private int birthYear;
+
+    public Person(){
+    }
+
+    public Person(String name){
+        this.name = name;
+    }
+
 }
